ec107992bda4bed3df9ac8042b8c4628cdad2011
RiccardoSansone <r.sansone@gmail.com>
2023-08-29 15:09:18 +0200

    complete jsnack2

---
+++ jsnack2/assets/js/script.js
@@ -0,0 +1,10 @@
+const wordOne = prompt('inserisci una parola a tuo piacere');
+const wordTwo = prompt('per favore, inserisci nuovamente una parola a tuo piacere');
+const printer = document.querySelector('h1');
+if(wordOne.length > wordTwo.length){
+    printer.innerHTML = wordOne + ' ' + wordTwo;
+} else if(wordOne.length < wordTwo.length){
+    printer.innerHTML = wordTwo + ' ' + wordOne;
+} else {
+    printer.innerHTML = 'HANNO LA STESSA LUNGHEZZA' + ' ' + wordOne + ' ' + wordTwo;
+}
