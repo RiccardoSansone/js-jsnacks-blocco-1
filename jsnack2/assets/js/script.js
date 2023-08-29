20854531667b6689fd0a9a2f445d7c68e5084a2e
RiccardoSansone <r.sansone@gmail.com>
2023-08-29 15:35:50 +0200

    fix order because i misread the request

--- jsnack2/assets/js/script.js
+++ jsnack2/assets/js/script.js
@@ -2,9 +2,9 @@ const wordOne = prompt('inserisci una parola a tuo piacere');
 const wordTwo = prompt('per favore, inserisci nuovamente una parola a tuo piacere');
 const printer = document.querySelector('h1');
 if(wordOne.length > wordTwo.length){
-    printer.innerHTML = wordOne + ' ' + wordTwo;
-} else if(wordOne.length < wordTwo.length){
     printer.innerHTML = wordTwo + ' ' + wordOne;
+} else if(wordOne.length < wordTwo.length){
+    printer.innerHTML = wordOne + ' ' + wordTwo;
 } else {
     printer.innerHTML = 'HANNO LA STESSA LUNGHEZZA' + ' ' + wordOne + ' ' + wordTwo;
 }
